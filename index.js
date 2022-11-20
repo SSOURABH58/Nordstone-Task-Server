d836230d3dd27e3d46bfedece81a259358b85cb0
SSOURABH58 <ssourabh025@gamil.com>
2022-11-20 23:19:54 +0530

    added notification api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express()
 app.use(cors());
@@ -23,6 +24,42 @@ const getAnswer = (num1, num2, opera) => {
     }
 }
 
+const notifyDelay = 5
+
+export const sendPushNotification = async (registrationToken) => {
+    var data = JSON.stringify({
+        "priority": "HIGH",
+        "data": {},
+        "notification": {
+            "body": `its a ${notifyDelay}s dilated notification`,
+            "title": "🎉 you are hired !!",
+            "vibrate": 1,
+            "sound": 1,
+            "show_in_foreground": true
+        },
+        "to": registrationToken
+    });
+
+    var config = {
+        method: 'post',
+        url: 'https://fcm.googleapis.com/fcm/send',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'key=AAAAHy8xf3E:APA91bFQcYo1x8tzqnR8etmmP-XSjSs1hIpRw0r5j1274AbMwQnvQbf1gPNtLvDji56IyYrsSAPltPKgZNXe5EqHdYHrDYQCcXcb0UGht-X93hYqkxp37xn9CjnqfcNeP0FCj7paS8fW'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
+
 
 app.get('/', (req, res) => {
     res.send('Hello Nordstone');
@@ -36,6 +73,16 @@ app.post('/calc', (req, res) => {
     console.log(ans);
 
     res.end(String(ans));
+});
+
+app.post('/notify', async (req, res) => {
+    const { token } = req.body;
+    console.log("req", req.body);
+    setTimeout(() => {
+
+        sendPushNotification(token)
+    }, notifyDelay * 1000);
+    res.end();
 });
 
 app.listen(process.env.PORT || 3000,
